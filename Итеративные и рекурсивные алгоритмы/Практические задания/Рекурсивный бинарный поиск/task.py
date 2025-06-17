2f84663d781b4d3497739286d2d1d76bfd3ea4c2
Walrus19 <dick19@yandex.ru>
2025-06-17 15:41:48 +0300

    3

--- Итеративные и рекурсивные алгоритмы/Практические задания/Рекурсивный бинарный поиск/task.py
+++ Итеративные и рекурсивные алгоритмы/Практические задания/Рекурсивный бинарный поиск/task.py
@@ -16,4 +16,34 @@ def binary_search(
     :raise: ValueError если элемента нет в массиве
     :return: Индекс элемента в массиве
     """
+    if right_border is None:
+        right_border = len(seq) - 1
+
+    if left_border > right_border:
+        raise ValueError('')
+
+    ind_pivot = (right_border + left_border) // 2
+    current_value = seq[ind_pivot]
+
+    if current_value == value:
+        while True:
+            if ind_pivot - 1 >= 0 and seq[ind_pivot - 1] == value:
+                ind_pivot -= 1
+            else:
+                return ind_pivot
+
+        # while True:
+        #     if not 0 <= ind_pivot - 1 < len(seq) or seq[ind_pivot - 1] != value:
+        #         return ind_pivot
+        #     else:
+        #         ind_pivot -= 1
+
+    if current_value > value:
+        right_border = ind_pivot - 1
+    else:
+        left_border = ind_pivot + 1
+    # print(ind_a, ind_b)
+    return binary_search(value, seq,  left_border, right_border)
+
     ...  # TODO реализовать алгоритм бинарного поиска
+
