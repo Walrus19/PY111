fefea5fd668da459941bdad2c00881609e17028c
Walrus19 <dick19@yandex.ru>
2025-06-18 21:00:15 +0300

    cl3

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
@@ -5,4 +5,14 @@ def factorial_recursive(n: int) -> int:
     :param n: Число, факториал которого нужно найти
     :return: n! - факториал числа n
     """
-    ...  # TODO реализовать рекурсивный алгоритм нахождения факториала
+     # TODO реализовать рекурсивный алгоритм нахождения факториала
+    if not isinstance(n, int):
+        raise TypeError()
+    if n < 0:
+        raise ValueError()
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n-1)
+
+# print(factorial_recursive(5))
+
