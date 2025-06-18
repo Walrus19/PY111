1577fe6bc62c5d5f4f4e7e3d60227d0cfc060c9c
Walrus19 <dick19@yandex.ru>
2025-06-18 21:01:16 +0300

    cl4

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Рекурсивный факториал/task.py
@@ -10,9 +10,10 @@ def factorial_recursive(n: int) -> int:
         raise TypeError()
     if n < 0:
         raise ValueError()
+
     if n == 0:
         return 1
     return n * factorial_recursive(n-1)
 
-# print(factorial_recursive(5))
+ print(factorial_recursive(5))
 
