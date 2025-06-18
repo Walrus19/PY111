625d105eb0ed3f18647b51a480acdce08653cd0c
Walrus19 <dick19@yandex.ru>
2025-06-18 20:29:29 +0300

    cl2

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный линейный поиск/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный линейный поиск/task.py
@@ -11,4 +11,18 @@ def min_search(arr: List[int]) -> int:
     :param arr: Массив целых чисел
     :return: Индекс первого вхождения элемента в массиве
     """
-    ...  # TODO реализовать итеративный линейный поиск
+     # TODO реализовать итеративный линейный поиск
+
+    if not arr:
+        raise ValueError()
+
+    min = arr[0]
+    index = 0
+    for idx, value in enumerate(arr):
+        if value < min:
+            min = value
+            index = idx
+
+    return index
+# arr= [4,2,3,1,5,5,8]
+# print(min_search(arr))
