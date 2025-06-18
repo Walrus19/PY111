3ee2f481a4c928bde0e13f0ebb31658f91bc73fd
Walrus19 <dick19@yandex.ru>
2025-06-18 20:43:50 +0300

    cl3

--- Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
+++ Итеративные и рекурсивные алгоритмы/Лабораторная 2/Итеративный факториал/task.py
@@ -5,4 +5,12 @@ def factorial_iterative(n: int) -> int:
     :param n: Число, факториал которого нужно найти
     :return: n! - факториал числа n
     """
-    ...  # TODO реализовать итеративный алгоритм нахождения факториала
+     # TODO реализовать итеративный алгоритм нахождения факториала
+
+    rez = 1
+    for i in range(1,n+1):
+        rez *= i
+
+    return rez
+
+print(factorial_iterative(5))
