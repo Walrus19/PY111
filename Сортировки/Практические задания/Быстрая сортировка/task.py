c8207cad63e0aa14fd6b4dd7c079963604ebbcd5
Walrus19 <dick19@yandex.ru>
2025-06-23 09:29:42 +0300

    23

--- Сортировки/Практические задания/Быстрая сортировка/task.py
+++ Сортировки/Практические задания/Быстрая сортировка/task.py
@@ -13,3 +13,94 @@ def sort(container: List[int]) -> List[int]:
     :return: Отсортированная в порядке возрастания последовательность
     """
     ...  # TODO реализовать алгоритм быстрой сортировки
+    if len(container) <= 1:
+        return container
+    pivot = container[len(container) // 2]
+
+    """1-решение"""
+    # left = [x for x in container if x < pivot]
+    # middle = [x for x in container if x == pivot]
+    # right = [x for x in container if x > pivot]
+
+    """2-решение"""
+    new_container = container.copy()
+    left = []
+    for x in container:
+        if x < pivot:
+            left.append(x)
+            new_container.remove(x)
+
+    container = new_container
+    middle = []
+    for x in container:
+        if x == pivot:
+            middle.append(x)
+            new_container.remove(x)
+
+    right = new_container.copy()
+
+    return sort(left) + middle + sort(right)
+
+
+def partition(arr, low, high):
+    pivot = arr[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i + 1
+
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+
+        quick_sort(arr, low, pivot_index-1)
+        quick_sort(arr, pivot_index + 1, high)
+
+
+def sort(container: List[int]) -> List[int]:
+    """
+    Алгоритм быстрой сортировки.
+
+    1. Выбираем опорный элемент. Например, первый элемент.
+    2. В левую часть отправляем всё что меньше опорного элемента, в правую всё что больше.
+    3. К левой и правой части рекурсивно применяет алгоритм быстрой сортировки.
+
+    :param container: последовательность, которую надо отсортировать
+    :return: Отсортированная в порядке возрастания последовательность
+    """
+    container = container.copy()
+    quick_sort(container, 0, len(container) - 1)  # TODO реализовать алгоритм быстрой сортировки
+    return container
+
+# a = [4,3,5,2,7,5]
+# print(sort(a))
+# print(a)
+#
+# sorted([4,3,5,2,7,5])
+# [4,3,5,2,7,5].sort()
+
+# print(sort([4,3,5,2,7,5]))
+#
+# a = [4,3,5,2,7,5]
+# print(a)
+# quick_sort(a, 0, len(a) - 1)
+# print(a)
+
+# print(a)
+# index_part = partition(a, 0, len(a)-1)
+# print(index_part)
+# print(a)
+# index_part = partition(a, 0, index_part-1)
+# print(index_part)
+# print(a)
+# index_part = partition(a, index_part+1, 3)
+# print(index_part)
+# print(a)
+
--- Сортировки/Практические задания/Сортировка слиянием/task.py
+++ Сортировки/Практические задания/Сортировка слиянием/task.py
@@ -1,6 +1,21 @@
 from typing import List
 
 
+def merge(arr_a: list, arr_b: list) -> list:
+    res = []
+    while True:
+        if arr_a[0] <= arr_b[0]:
+            res.append(arr_a.pop(0))
+        else:
+            res.append(arr_b.pop(0))
+        if not arr_a:
+            res.extend(arr_b)
+            return res
+        elif not arr_b:
+            res.extend(arr_a)
+            return res
+
+
 def sort(container: List[int]) -> List[int]:
     """
     Алгоритм сортировки слиянием.
@@ -13,3 +28,23 @@ def sort(container: List[int]) -> List[int]:
     :return: Отсортированный в порядке возрастания массив
     """
     ...  # TODO реализуйте сортировку слиянием
+    if len(container) in (0, 1):
+        return container
+
+    mid_ind = len(container) // 2
+    arr_a = container[:mid_ind]
+    arr_b = container[mid_ind:]
+
+    sort_arr_a = sort(arr_a)
+    sort_arr_b = sort(arr_b)
+
+    return merge(sort_arr_a, sort_arr_b)
+
+
+
+# print(merge([1,2], [4,5]))
+# print(merge([1,6], [2, 5, 7, 20]))
+# print(merge([6,7], [4,5]))
+# print(merge([6], [5]))
+
+# print(sort([4,3,5,2,7]))
